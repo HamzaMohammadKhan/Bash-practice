56ff456912c2ed5171467e1ddda987ab56ce8072
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2023-02-19 20:26:44 +0500

    Add files via upload

---
+++ medium_question_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Exercise_2 - Modify the shell script from exercise 1 to include a variable. The variable will hold the contents of the message “Shell Scripting is Fun!”
+
+
+sentence="Shell scripting is fun"
+echo $sentence
---
+++ medium_question_3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Exercise_3 - Store the output of the command “hostname” in a variable. Display “This script is running on _.” where “_” is the output of the “hostname” command.
+
+
+hostname=hostname
+hostname1=$(hostname)
+echo $hostname
+echo $hostname1
